1e549ec45d12bca2399b6598e1c8688e1c7fb67e
Sofiia-cloud <sofiia.g@icloud.com>
2025-10-21 19:39:51 +0300

    add new endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,20 @@
 import express from 'express';
 
+const ideas = [
+  { nick: 'nick-1', name: 'Idea 1', description: 'Description of idea 1' },
+  { nick: 'nick-2', name: 'Idea 2', description: 'Description of idea 2' },
+  { nick: 'nick-3', name: 'Idea 3', description: 'Description of idea 3' },
+  { nick: 'nick-4', name: 'Idea 4', description: 'Description of idea 4' },
+  { nick: 'nick-5', name: 'Idea 5', description: 'Description of idea 5' },
+];
+
 const expressApp = express();
 expressApp.get('/ping', (req, res) => {
   res.send('pong');
+});
+
+expressApp.get('/ideas', (req, res) => {
+  res.send(ideas);
 });
 
 expressApp.listen(3000, () => {
