3f075bb5c2a095d10442299ccc61e5ff9e630ffc
Sofiia-cloud <sofiia.g@icloud.com>
2025-11-13 19:01:28 +0300

    Add trpc procedure

--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,19 +1,26 @@
 import { useParams } from 'react-router-dom';
 
-import type { ViewIdeaRouteParams } from '../../lib/routes';
+import { type ViewIdeaRouteParams } from '../../lib/routes';
+import { trpc } from '../../lib/trpc';
 
 export const ViewIdeaPage = () => {
   const { ideaNick } = useParams() as ViewIdeaRouteParams;
+  const { data, error, isLoading, isFetching, isError } = trpc.getIdea.useQuery({ ideaNick });
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>;
+  }
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+  if (!data?.idea) {
+    return <span>Idea not found</span>;
+  }
   return (
     <div>
-      <h1>{ideaNick}</h1>
+      <h1>{data.idea.name}</h1>
 
-      <p>Description of the idea</p>
-      <div>
-        <p>Text paragraph... </p>
-        <p>Text paragraph... </p>
-        <p>Text paragraph... </p>
-      </div>
+      <p>{data.idea.description}</p>
+      <div dangerouslySetInnerHTML={{ __html: data.idea.text }}></div>
     </div>
   );
 };
